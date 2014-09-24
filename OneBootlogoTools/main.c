3b75b89a7db9767442a9606f783f05816ff349e7
Matteo Gaggiano <maxxxx92@gmail.com>
2014-09-24 10:58:34 +0200

    Added support for single action Now the program customize an existing logo.bin (this can preserve other images) Usage function added

--- OneBootlogoTools/main.c
+++ OneBootlogoTools/main.c
@@ -9,12 +9,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 #if DEBUG
 #define DLOG(fmt, ...) printf("Debug: " fmt "\n", ##__VA_ARGS__);
 #else
 #define DLOG(fmt, ...) do{} while(0)
 #endif
+
+void usage();
+
+typedef unsigned short bool;
+#define FALSE 0
+#define TRUE  1
 
 unsigned long FILE_LEN = 9821696;
 unsigned long LOGO_LEN = 6220800;
@@ -28,9 +35,17 @@ uint8_t headerLogo[] = {
     0x00, 0x52, 0x94, 0x00, 0x00, 0x5E, 0x60, 0x00,
     0x00, 0x68, 0x73, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
+uint8_t header2[] = {};
+
+uint8_t header3[] = {};
+
 uint8_t headerFast[] = {
     0x53, 0x50, 0x4C, 0x41, 0x53, 0x48, 0x21, 0x21,
     0x5E, 0x01, 0x00, 0x00, 0x39, 0x01, 0x00, 0x00 };
+
+uint8_t header5[] = {};
+
+uint8_t header6[] = {};
 
 int main(int argc, const char * argv[]) {
     
@@ -42,66 +57,109 @@ int main(int argc, const char * argv[]) {
     FILE *fastF = NULL;
     FILE *fileF = NULL;
     
+    bool flashBoot = FALSE;
+    bool flashFast = FALSE;
+    
     uint8_t *logoBin = malloc(sizeof(uint8_t) * LOGO_LEN);
     
     uint8_t *fastBin = malloc(sizeof(uint8_t) * FAST_LEN);
     
     uint8_t *fileBin = malloc(sizeof(uint8_t) * FILE_LEN);
     
+    if (argc == 2) {
+        if (!strcmp(argv[1], "-b")) {
+            flashBoot = TRUE;
+        } else if (!strcmp(argv[1], "-f")) {
+            flashFast = TRUE;
+        }
+    } else if (argc == 3) {
+        if (!strcmp(argv[1], "-b")) {
+            flashBoot = TRUE;
+        } else if (!strcmp(argv[1], "-f")) {
+            flashFast = TRUE;
+        }
+        if (!strcmp(argv[2], "-b")) {
+            flashBoot = TRUE;
+        } else if (!strcmp(argv[2], "-f")) {
+            flashFast = TRUE;
+        }
+    } else {
+        printf("Too many arguments!\n");
+        usage();
+        return 255;
+    }
+    
+    
+    
     logoF = fopen("bootlogo.raw", "r");
-    if (!logoF) {
+    if (!logoF && flashBoot) {
         perror("Error while read bootlogo.raw");
         return errno;
     }
+        flashBoot = TRUE;
     
     fastF = fopen("fastboot.raw", "r");
-    if (!fastF) {
+    if (!fastF && flashFast) {
         perror("Error while read fastboot.raw");
         return errno;
     }
     
+    fileF = fopen("logo.bin", "r");
+    if (!fileF) {
+        perror("Error while read logo.bin");
+        return errno;
+    }
     
-    fseek(logoF, 0L, SEEK_END);
-    logosize = ftell(logoF);
+    if (flashBoot) {
+        fseek(logoF, 0L, SEEK_END);
+        logosize = ftell(logoF);
+        
+        fseek(logoF, 0L, SEEK_SET);
+    }
     
-    fseek(logoF, 0L, SEEK_SET);
+    if (flashFast) {
+        fseek(fastF, 0L, SEEK_END);
+        fastsize = ftell(fastF);
+        
+        fseek(fastF, 0L, SEEK_SET);
+    }
     
-    fseek(fastF, 0L, SEEK_END);
-    fastsize = ftell(fastF);
+    for (long int i = 0; i < FILE_LEN; i++) {
+        fileBin[i] = fgetc(fileF);
+    }
     
-    fseek(fastF, 0L, SEEK_SET);
-    
-    
-    for (long int i = 0; i < LOGO_LEN; i++) {
+    fclose(fileF);
+
+    for (long int i = 0; i < LOGO_LEN && flashBoot; i++) {
         logoBin[i] = fgetc(logoF);
     }
     
-    for (long int i = 0; i < FAST_LEN; i++) {
+    for (long int i = 0; i < FAST_LEN && flashFast; i++) {
         fastBin[i] = fgetc(fastF);
     }
     
     for (long long int i = 0; i < FILE_LEN; i++)
     {
-        if (i >= 0 && i < 48) {
-            fileBin[i] = headerLogo[i];
-        }
+        if (i >= 0 && i < 48 && flashBoot) {
+            fileBin[i] = headerLogo[i]; // main logo header
+        } else
         
-        if (i >= 48 && i < 512) {
+        if (i >= 48 && i < 512 && flashBoot) { // main logo header
             fileBin[i] = 0x00;
-        }
+        } else
         
-        if (i >= 512 && i < 6221312)
+        if (i >= 512 && i < 6221312 && flashBoot)
         {
-            fileBin[i] = logoBin[i - 512];
-        }
+            fileBin[i] = logoBin[i - 512]; // bootlogo image
+        } else
         
-        if (i >= 7234048 && i < 7234064) {
-            fileBin[i] = headerFast[i - 7234048];
-        }
+        if (i >= 7234048 && i < 7234064 && flashFast) {
+            fileBin[i] = headerFast[i - 7234048]; // fastboot logo header
+        } else
         
-        if (i >= 7234560 && i < 7549560)
+        if (i >= 7234560 && i < 7549560 && flashFast)
         {
-            fileBin[i] = fastBin[i - 7234560];
+            fileBin[i] = fastBin[i - 7234560]; // fastboot logo image
         }
     }
     
@@ -122,4 +180,8 @@ int main(int argc, const char * argv[]) {
     free(fastBin);
     
     return 0;
+}
+
+void usage() {
+    printf("OneBootlogoTools:\n\t-b bootlogo.raw \t Overwrite the bootlogo\n\t-f fastboot.raw \t Overwrite the fastboot image\n\nVersion: 1.0b1 by Matteo Gaggiano\n");
 }
